023541416b06c390c3cf51ce82941e031b7d3bad
DogGarY <168201017+DogGarY@users.noreply.github.com>
2025-05-07 14:16:13 +0300

    script

---
+++ script.bat
@@ -0,0 +1,20 @@
+echo off
+md Chelushkin
+cd Chelushkin
+md Artem
+md Alexandrovich
+pause
+echo > 10022005.txt
+cd Alexandrovich
+echo > acer.txt
+cd ../
+cd ../
+pause
+del Chelushkin /S /Q /F
+pause 
+cd Chelushkin
+rd Artem
+rd Alexandrovich
+cd ../
+rd Chelushkin
+pause
